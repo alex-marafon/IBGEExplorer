7c22c0e3413371f9445625820e0b7cc7f4767c8b
Brewerton Santos <brewerton.santos@outlook.com>
2023-10-12 20:55:42 -0300

    adição do escopo de testes unitarios.

---
+++ Tests/IBGEExplorer.Tests/Contexts/FileManagerTests.cs
@@ -0,0 +1,70 @@
+﻿namespace IBGEExplorer.Tests.Contexts;
+
+public class FileManagerTests
+{
+    [Fact]
+    public void ShouldNotAcceptInvalidExtensions()
+    {
+        Assert.Fail("");
+    }
+
+    [Fact]
+    public void ShouldNotAcceptLargeFiles()
+    {
+        Assert.Fail("");
+    }
+
+    [Fact]
+    public void ShouldNotAcceptEmptyFiles()
+    {
+        Assert.Fail("");
+    }
+
+    [Fact]
+    public void ShouldNotAcceptFilesWithInvalidHeaders()
+    {
+        Assert.Fail("");
+    }
+    
+    [Fact]
+    public void ShouldNotAcceptFilesWithInvalidFields()
+    {
+        Assert.Fail("");
+    }
+
+    [Fact]
+    public void ShouldNotAcceptCorruptedFiles()
+    {
+        Assert.Fail("");
+    }
+
+    [Fact]
+    public void ShouldNotAcceptRepeatedFIles()
+    {
+        Assert.Fail("");
+    }
+
+    [Fact]
+    public void ShouldAcceptFilesWithDifferentColunmOrders()
+    {
+        Assert.Fail("");
+    }
+    
+    [Fact]
+    public void ShouldImportAValidFile()
+    {
+        Assert.Fail("");
+    }
+
+    [Fact]
+    public void ShouldNotImportWhenAnonymous()
+    {
+        Assert.Fail("");
+    }
+
+    [Fact]
+    public void ShouldNotImportWhenNotAdmin()
+    {
+        Assert.Fail("");
+    }
+}
