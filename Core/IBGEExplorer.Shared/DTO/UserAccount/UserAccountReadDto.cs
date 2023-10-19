b7f68348521a3ada41a9be6432400f46fba18569
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2023-10-19 09:05:50 -0300

    fix: pasta de DTO deletada (criada erroneamente)

--- Core/IBGEExplorer.Shared/DTO/UserAccount/UserAccountReadDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace IBGEExplorer.Shared.DTO.UserAccount;
-
-public class UserAccountReadDto
-{
-    public string Email { get; set; } = null!;
-    public string Token { get; set; } = null!;
-}
