aa25d44f8b15489d78b6851b6f1e1f824c76476a
Alex Marafon <alex-asam@hotmail.com>
2023-10-19 23:45:06 -0400

    Create projeto base City

---
+++ Core/IBGEExplorer.City/Entities/City.cs
@@ -0,0 +1,12 @@
+﻿
+using IBGEExplorer.Shared.Entities;
+
+namespace IBGEExplorer.City.Entities;
+
+public class City : Entity<int>
+{
+    public string IdCode { get; set; }
+    public string CityName { get; set; }
+    public string StateName { get; set; }
+
+}
